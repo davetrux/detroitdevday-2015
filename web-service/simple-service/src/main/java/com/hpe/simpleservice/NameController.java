6d343fcf4238d8f1ec01b524a4756ce9e7a46a4f
David Truxall <davetrux@gmail.com>
2015-11-09 23:14:27 -0500

    Working unit tests

--- web-service/simple-service/src/main/java/com/hpe/simpleservice/NameController.java
+++ web-service/simple-service/src/main/java/com/hpe/simpleservice/NameController.java
@@ -2,12 +2,12 @@ package com.hpe.simpleservice;
 
 import com.hpe.simpleservice.error.ValidationException;
 import com.hpe.simpleservice.error.Web409Exception;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.naming.NamingException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,44 +21,45 @@ import java.util.Map;
 @RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
 public class NameController {
 
+    @Autowired
     private NameGenerator _generator;
 
-    public NameController() throws NamingException {
-        _generator = new NameGenerator();
+    public NameController() {
+
     }
 
     @RequestMapping(value = "/names/{count}", method = RequestMethod.GET, produces = {"application/json"})
-        @ResponseBody
-        public List<Person> getPersonList(@PathVariable("count") int count) throws ValidationException {
+    @ResponseBody
+    public List<Person> getPersonList(@PathVariable("count") int count) throws ValidationException {
 
-            if (!countValid(count)) {
-                throw new Web409Exception("count");
-            }
-
-            return _generator.getRandomPersons(count);
+        if (!countValid(count)) {
+            throw new Web409Exception("count");
         }
 
+        return _generator.getRandomPersons(count);
+    }
 
-        @RequestMapping(value = "/name", method = RequestMethod.GET,  produces = {"application/json"})
-        @ResponseBody
-        public Person getSingleName() {
 
-            return _generator.getRandomName();
-        }
+    @RequestMapping(value = "/name", method = RequestMethod.GET,  produces = {"application/json"})
+    @ResponseBody
+    public Person getSingleName() {
 
-        private boolean countValid(int count) {
-            return count > 0 && count < 101;
-        }
+        return _generator.getRandomName();
+    }
 
-        @ExceptionHandler(Web409Exception.class)
-        @ResponseStatus(HttpStatus.CONFLICT)
-        @ResponseBody public Map<String,Object> handle409Exception(Web409Exception bre) {
-            return createErrorHash(bre.getMessage());
-        }
+    private boolean countValid(int count) {
+        return count > 0 && count < 101;
+    }
 
-        private Map<String, Object> createErrorHash(String message) {
-            Map<String, Object> result = new HashMap<>();
-            result.put("message", message);
-            return result;
-        }
+    @ExceptionHandler(Web409Exception.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ResponseBody public Map<String,Object> handle409Exception(Web409Exception bre) {
+        return createErrorHash(bre.getMessage());
+    }
+
+    private Map<String, Object> createErrorHash(String message) {
+        Map<String, Object> result = new HashMap<>();
+        result.put("message", message);
+        return result;
+    }
 }
--- web-service/simple-service/src/main/java/com/hpe/simpleservice/NameGenerator.java
+++ web-service/simple-service/src/main/java/com/hpe/simpleservice/NameGenerator.java
@@ -1,84 +1,79 @@
 package com.hpe.simpleservice;
 
-import javax.naming.InitialContext;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-import javax.naming.Context;
-import javax.naming.NamingException;
 /**
  * @author truxall
  */
 public class NameGenerator {
 
-    private static final String CONTEXT_NAME = "java:comp/env";
 
     private String mGender;
 
-    public NameGenerator() throws NamingException {
-        Context context = (Context) new InitialContext().lookup(CONTEXT_NAME);
-        mGender = (String) context.lookup("gender");
+    public NameGenerator(String gender)  {
+        mGender = gender;
     }
 
     public Person getRandomName() {
-            Person result = new Person();
-            Random generator = new Random();
+        Person result = new Person();
+        Random generator = new Random();
 
-            int r = generator.nextInt(Constants.getSurnames().length);
+        int r = generator.nextInt(Constants.getSurnames().length);
 
-            result.setLastName(Constants.getSurnames()[r]);
+        result.setLastName(Constants.getSurnames()[r]);
 
-            if(mGender.equals(Constants.FEMALE)){
-                r =  generator.nextInt(Constants.getFemale().length);
-                result.setFirstName(Constants.getFemale()[r]);
-                result.setGender("f");
-            } else {
-                r =  generator.nextInt(Constants.getMale().length);
-                result.setFirstName(Constants.getMale()[r]);
-                result.setGender("m");
-            }
-
-            return result;
-        }
-
-        public Person getRandomName(String gender) {
-            Person result = new Person();
-            Random generator = new Random();
-
-            int r = generator.nextInt(Constants.getSurnames().length);
-
-            result.setLastName(Constants.getSurnames()[r]);
-
-            if(gender.equalsIgnoreCase("f")){
-                r =  generator.nextInt(Constants.getFemale().length);
-                result.setFirstName(Constants.getFemale()[r]);
-                result.setGender("f");
-            } else {
-                r =  generator.nextInt(Constants.getMale().length);
-                result.setFirstName(Constants.getMale()[r]);
-                result.setGender("m");
-            }
-
-            return result;
-        }
-
-        public List<Person> getRandomPersons(int count) {
-            List<Person> result = new ArrayList<Person>();
-
-            for(int i = 0; i < count; i++){
-                result.add(getRandomName());
-            }
-
-            return result;
-        }
-
-        private Person getStaticPerson() {
-            Person result = new Person();
-
-            result.setFirstName("Dave");
-            result.setLastName("Truxall");
+        if(mGender.equals(Constants.FEMALE)){
+            r =  generator.nextInt(Constants.getFemale().length);
+            result.setFirstName(Constants.getFemale()[r]);
+            result.setGender("f");
+        } else {
+            r =  generator.nextInt(Constants.getMale().length);
+            result.setFirstName(Constants.getMale()[r]);
             result.setGender("m");
-            return result;
         }
+
+        return result;
+    }
+
+    public Person getRandomName(String gender) {
+        Person result = new Person();
+        Random generator = new Random();
+
+        int r = generator.nextInt(Constants.getSurnames().length);
+
+        result.setLastName(Constants.getSurnames()[r]);
+
+        if(gender.equalsIgnoreCase("f")){
+            r =  generator.nextInt(Constants.getFemale().length);
+            result.setFirstName(Constants.getFemale()[r]);
+            result.setGender("f");
+        } else {
+            r =  generator.nextInt(Constants.getMale().length);
+            result.setFirstName(Constants.getMale()[r]);
+            result.setGender("m");
+        }
+
+        return result;
+    }
+
+    public List<Person> getRandomPersons(int count) {
+        List<Person> result = new ArrayList<Person>();
+
+        for(int i = 0; i < count; i++){
+            result.add(getRandomName());
+        }
+
+        return result;
+    }
+
+    private Person getStaticPerson() {
+        Person result = new Person();
+
+        result.setFirstName("Dave");
+        result.setLastName("Truxall");
+        result.setGender("m");
+        return result;
+    }
 }
