a6c2624158adc51948e60b2d0f67bd4ad093b26a
Dave Truxall <david.truxall@hp.com>
2015-11-13 23:04:33 -0500

    Transfer to repo

---
+++ android/simpleconsumer/app/src/debug/java/com/hpe/simpleconsumer/rest/Constants.java
@@ -0,0 +1,11 @@
+package com.hpe.simpleconsumer.rest;
+
+/**
+ * @author trux
+ *
+ * 11/13/15
+ */
+public class Constants {
+    public static final String SERVICE_URL = "http://15.126.220.46/dev/api/";
+    public static final String SERVICE_RECORDS = "/names/5";
+}
